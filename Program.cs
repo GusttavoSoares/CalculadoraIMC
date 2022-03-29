e60a1974baf8257aa883cd7a71779b1c951376e9
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-03-29 15:17:12 -0300

    Adicionando a funcionalidade de apresentação da faixa do usuário

--- Program.cs
+++ Program.cs
@@ -15,12 +15,33 @@ namespace CalculadoraIMC
             Console.WriteLine ("Qual o seu peso em Kg ? Ex: 70,3");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso / (altura*altura);
-
+            double imc = Math.Round(peso / (altura*altura), 2);
+            
             Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura,peso);
             Console.WriteLine("Seu IMC é {0}", imc);
 
-
+            if (imc < 17){
+                Console.WriteLine ("Muito abaixo do peso");
+                }
+                else if (imc <= 18.49) {
+                    Console.WriteLine ("Abaixo do peso");
+                }
+                else if (imc <= 24.99){
+                    Console.WriteLine ("Peso normal");
+                }
+                 else if (imc <= 29.99){
+                    Console.WriteLine ("Acima do peso");
+                }
+                   else if (imc <= 34.99){
+                    Console.WriteLine ("Obesidade I");
+                }
+                else if (imc <= 39.99){
+                    Console.WriteLine ("Obesidade II (severa)");
+                }
+                else if (imc >= 40){
+                    Console.WriteLine ("Obesidade II (mórbida)");
+                }
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Aplicação console para cálculo do IMC.
 ## Funcionalidades
 - Calcula e apresenta o IMC
+- Apresenta a situação do usuário
 
 ## Tecnologias utilizadas
 
